93d139fe7a2af04954a204948f57ed0f31a2365c
Michal Gebski <gebski.michal@gmail.com>
2023-05-07 11:15:57 +0200

    3. @Pre i @Roles

--- src/main/java/pl/sda/springsecurityapp/DummyController.java
+++ src/main/java/pl/sda/springsecurityapp/DummyController.java
@@ -1,9 +1,12 @@
 package pl.sda.springsecurityapp;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 @RequestMapping("/hello")
@@ -14,11 +17,13 @@ public class DummyController {
         return ResponseEntity.ok("Hello from my api!");
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/forAdmin")
     public ResponseEntity<String> helloWorldForAdmin() {
         return ResponseEntity.ok("Witaj adminie!");
     }
 
+    @RolesAllowed("ROLE_USER")
     @GetMapping("/forUser")
     public ResponseEntity<String> helloWorldForUser() {
         return ResponseEntity.ok("Witaj użytkowniku!");
--- src/main/java/pl/sda/springsecurityapp/config/SecurityConfig.java
+++ src/main/java/pl/sda/springsecurityapp/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.sda.springsecurityapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +13,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
 //    @Bean
@@ -29,8 +31,8 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable().cors().and()
                 .authorizeRequests((authz) -> authz
-                        .antMatchers("/hello/forAdmin/**").hasRole("ADMIN")
-                        .antMatchers("/hello/forUser/**").hasRole("USER")
+//                        .antMatchers("/hello/forAdmin/**").hasRole("ADMIN")
+//                        .antMatchers("/hello/forUser/**").hasRole("USER")
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
